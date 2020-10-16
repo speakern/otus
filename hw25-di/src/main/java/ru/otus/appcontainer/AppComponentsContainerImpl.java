9480574a69afd02bf47d576f05ea379aab343df9
speakern <hobbymantop@gmail.com>
2020-10-16 08:27:28 +0300

    find Collection

--- hw25-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw25-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -8,6 +8,7 @@ import ru.otus.appcontainer.api.AppComponentsContainerConfig;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -47,10 +48,34 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
             methods.addAll(Arrays.asList(methodsAll));
 
         for (Method method : methods) {
-            System.out.println(method.getName());
-            System.out.println(((Class) method.getGenericReturnType()).getSimpleName());
-        }
+            if (method.getDeclaredAnnotation(AppComponent.class) == null) continue;
 
+            System.out.println(method.getName());
+           // System.out.println(((Class) method.getGenericReturnType()).getSimpleName());
+
+            String classMethodName = ((Class) method.getGenericReturnType()).getSimpleName();
+
+            Type[] types = method.getGenericParameterTypes();
+            Object resultObject = null;
+            if (types.length > 0) {
+                int sizeArray = types.length;
+                Object[] args = new Object[sizeArray];
+                for (int i = 0; i < types.length; i++) {
+                    //System.out.println("    " + ((Class) types[i]).getSimpleName());
+                    String classParameterName = ((Class) types[i]).getSimpleName();
+                    args[i] = appComponentsByName.get(classParameterName);
+                }
+                if (!checkArrayWithNull(args)) {
+                    resultObject = method.invoke(object, args);
+                }
+            } else {
+                resultObject = method.invoke(object);
+            }
+
+            if (resultObject != null) appComponentsByName.put(classMethodName, resultObject);
+        }
+        System.out.println("finish");
+        appComponentsByName.entrySet().forEach(e -> System.out.println(e.getKey() + "  " + e.getValue()));
     }
 
     private Comparator<Method> getMethodComparator(){
@@ -59,9 +84,17 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
             public int compare(Method m1, Method m2) {
                 AppComponent annotationM1 = m1.getDeclaredAnnotation(AppComponent.class);
                 AppComponent annotationM2 = m2.getDeclaredAnnotation(AppComponent.class);
+                int compareOrder = 0;
                 if ((annotationM1 != null) && (annotationM2 != null)) {
-                    return Integer.compare(annotationM1.order(), annotationM2.order());
+                    compareOrder = Integer.compare(annotationM1.order(), annotationM2.order());
+                    if (compareOrder == 0) {
+                        return annotationM1.name().compareTo(annotationM2.name());
+                    } else {
+                        return compareOrder;
+                    }
                 }
+                if (annotationM1 != null) return 1;
+                if (annotationM2 != null) return -1;
                 return 0;
             }
         };
@@ -82,4 +115,15 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     public <C> C getAppComponent(String componentName) {
         return null;
     }
+
+    private boolean checkArrayWithNull(Object[] objects) {
+        boolean result = false;
+        for (int i = 0; i < objects.length; i++) {
+            if (objects[i] == null) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
 }
--- hw25-di/src/main/java/ru/otus/config/AppConfig.java
+++ hw25-di/src/main/java/ru/otus/config/AppConfig.java
@@ -24,9 +24,9 @@ public class AppConfig {
         return new GameProcessorImpl(ioService, equationPreparer, playerService);
     }
 
-    @AppComponent(order = 0, name = "ioService")
-    public IOService ioService() {
-        return new IOServiceConsole(System.out, System.in);
-    }
+//    @AppComponent(order = 0, name = "ioService")
+//    public IOService ioService() {
+//        return new IOServiceConsole(System.out, System.in);
+//    }
 
 }
