bc837ac8666712a324686887e7aefb36e1e708f2
leha <speakern@rambler.ru>
2020-10-18 19:58:19 +0300

    hw26: combined hw24 with L26-warMvc

---
+++ hw26-warMvc/src/main/java/ru/otus/domain/PhoneDataSet.java
@@ -0,0 +1,79 @@
+package ru.otus.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tPhone")
+public class PhoneDataSet {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "phone_number", nullable = false)
+    private String number;
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public PhoneDataSet() {
+    }
+
+    public PhoneDataSet(String number, User user) {
+        this.number = number;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setPerson(User user) {
+        this.user = user;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", userId=" + user.getId() +
+                ", number='" + number + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PhoneDataSet phoneDataSet = (PhoneDataSet) o;
+        return Objects.equals(id, phoneDataSet.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
---
+++ hw26-warMvc/src/main/java/ru/otus/repository/UserDaoHibernate.java
@@ -0,0 +1,108 @@
+package ru.otus.repository;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.otus.domain.AddressDataSet;
+import ru.otus.domain.PhoneDataSet;
+import ru.otus.domain.User;
+import ru.otus.domain.UserDaoException;
+import ru.otus.sessionmanager.DatabaseSessionHibernate;
+import ru.otus.sessionmanager.SessionManager;
+import ru.otus.sessionmanager.SessionManagerHibernate;
+import ru.otus.utils.HibernateUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserDaoHibernate implements UserDao {
+    private static Logger logger = LoggerFactory.getLogger(UserDaoHibernate.class);
+    public static final String HIBERNATE_CFG_FILE = "hibernate.cfg.xml";
+
+    private final SessionManagerHibernate sessionManager;
+
+    public UserDaoHibernate() {
+        SessionFactory sessionFactory = HibernateUtils.buildSessionFactory(HIBERNATE_CFG_FILE, User.class, PhoneDataSet.class, AddressDataSet.class);
+
+        this.sessionManager = new SessionManagerHibernate(sessionFactory);
+    }
+
+
+    @Override
+    public Optional<User> findById(long id) {
+        DatabaseSessionHibernate currentSession = sessionManager.getCurrentSession();
+        try {
+            return Optional.ofNullable(currentSession.getHibernateSession().find(User.class, id));
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByLogin(String login) {
+        User user = sessionManager.getCurrentSession().getHibernateSession().createQuery(
+                "select u from User u where u.login = :login", User.class)
+                .setParameter("login", login)
+                .getSingleResult();
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public long insertUser(User user) {
+        DatabaseSessionHibernate currentSession = sessionManager.getCurrentSession();
+        try {
+            Session hibernateSession = currentSession.getHibernateSession();
+            hibernateSession.persist(user);
+            hibernateSession.flush();
+            return user.getId();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new UserDaoException(e);
+        }
+    }
+
+    @Override
+    public void updateUser(User user) {
+        DatabaseSessionHibernate currentSession = sessionManager.getCurrentSession();
+        try {
+            Session hibernateSession = currentSession.getHibernateSession();
+            hibernateSession.merge(user);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new UserDaoException(e);
+        }
+    }
+
+    @Override
+    public void insertOrUpdate(User user) {
+        DatabaseSessionHibernate currentSession = sessionManager.getCurrentSession();
+        try {
+            Session hibernateSession = currentSession.getHibernateSession();
+            if (user.getId() > 0) {
+                hibernateSession.merge(user);
+            } else {
+                hibernateSession.persist(user);
+                hibernateSession.flush();
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new UserDaoException(e);
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        Session session = sessionManager.getCurrentSession().getHibernateSession();
+        return session.createQuery("SELECT u FROM User u", User.class).getResultList();
+    }
+
+    @Override
+    public SessionManager getSessionManager() {
+        return sessionManager;
+    }
+}
