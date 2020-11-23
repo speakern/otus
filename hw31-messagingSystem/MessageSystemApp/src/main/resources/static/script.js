f5344f19bf9cbc97f60e9a18762100ba27d574da
leha <speakern@rambler.ru>
2020-11-23 06:50:10 +0300

    added allusers

--- hw31-messagingSystem/MessageSystemApp/src/main/resources/static/script.js
+++ hw31-messagingSystem/MessageSystemApp/src/main/resources/static/script.js
@@ -14,37 +14,31 @@ const connect = () => {
         console.log('Connected: ' + frame);
         // stompClient.subscribe('/topic/response.' + $("#roomId").val(), (message) => showMessage(JSON.parse(message.body).messageStr));
         stompClient.subscribe('/topic/response/getUser.1', (message) => showMessage(message.body));
-        stompClient.subscribe('/topic/response/getAllUsers.1', (message) => showMessage(message.body));
+        stompClient.subscribe('/topic/response/getAllUsers.1', (message) => showListUsers(JSON.parse(message.body)));
         sendMsgGetAllUsers();
     });
 }
 
 const showMessage = (message) => $("#userDataContainer").html(message)
 
-const showListUsers = (data) => {
+const showListUsers = (message) => {
+    //$("#list").html(message);
     let read_products_html = ``
-    $.each(data, function(key, val) {
+    $.each(message.list, function(key, val) {
         // создание новой строки таблицы для каждой записи
         read_products_html+=`
-                        <p><b>` + val.text + `</b></p>`
+                        <tr><td>` + val.id + `</td><td>`+ val.name +`</td><td>` + val.login  +`</td><td>` + val.password +`</td></tr>`
 
-        if (val.type == 1) {
-            $.each(val.answers, function(key1, val1) {
-                read_products_html += `
-                            <input name="` + val.id + `" type="radio" value="` + val1 + `">` + val1 + `</br>`
-            })
-        } else {
-            read_products_html += `
-                            <input name="` + val.id + `" maxlength="25" size="40" value=""></br>`
-        }
-        read_products_html+=`<br>`
+
+        // $.each(val.answers, function(key1, val1) {
+        //     read_products_html += `
+        //                 <input name="` + val.id + `" type="radio" value="` + val1 + `">` + val1 + `</br>`
+        // })
+
     });
     //read_products_html+=`<div><button type="submit" name="AddProduct">Отправить</button></div>`;
-    read_products_html+=`<p><input type="submit"></p>`;
-    //            read_products_html+=`<input type="hidden" th:name="${_csrf.parameterName}" th:value="${_csrf.token}" />`
 
-    //read_products_html+=`</form>`;
-    $("#send1").html(read_products_html);
+    $("#list").html(read_products_html);
 
 }
 const sendMsgGetUser = () => {
